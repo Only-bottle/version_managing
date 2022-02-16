6bb2b152be91c53457f65e9e6fd3aefb4a6b7cee
Byeongman Lee <33439998+Only-bottle@users.noreply.github.com>
2022-02-16 17:33:48 +0900

    feat: add person class (#53)

--- note.py
+++ note.py
@@ -33,3 +33,11 @@ peggy = Penguin()
 peggy.whoisThis()
 peggy.swim()
 peggy.run()
+
+class Person:
+    def say_hi(self):
+        print('Hello, how are you?')
+p = Person()
+p.say_hi()
+# The previous 2 lines can also be written as
+# Person().say_hi()
