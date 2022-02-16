5dfcc89dd89e917233e82d37226ad73d3a5d1f26
Byeongman Lee <33439998+Only-bottle@users.noreply.github.com>
2022-02-16 22:46:41 +0900

    release: v0.22.0 배포 (#64)
    * bump: version 0.20.1 → 0.21.0
    
    * feat: add release-drafter-config
    
    * bump: version 0.21.0 → 0.21.1
    
    * feat: add release-drafter.yml
    
    * bump: version 0.21.1 → 0.21.2
    
    * feat: remove code (#63)
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- note.py
+++ note.py
@@ -72,10 +72,3 @@ s = Student('Swaroop', 25, 75)
 print()
 members = [t, s]
 songs = [t, s]
-for member in members:
-    # Works for both Teachers and Students
-    member.tell()
-for song in songs:
-    # Works for both Teachers and Students
-    song.tell()
-print('hi')
